7a8b174e55dd8eb90bee79fd6da48cd770c06dfe
Owais <awaisajaz1@gmail.com>
2026-07-25 17:45:51 +0500

    audit logs tables added

--- pagila_dbt_analytics/macros/audit_sql/create_etl_run_log.sql
+++ pagila_dbt_analytics/macros/audit_sql/create_etl_run_log.sql
@@ -2,18 +2,17 @@
 
 {% set sql %}
 
-create table if not exists audit.etl_run_log
+create table audit.etl_run_log
 (
-    run_log_id          bigserial primary key,
-    batch_id            bigint not null,
-    invocation_id       varchar(100) not null,
-    model_name          varchar(100) not null,
-    layer_name          varchar(30),
-    started_at          timestamp,
-    finished_at         timestamp,
-    status              varchar(20),
-    rows_processed      bigint,
-    error_message       text
+    run_log_id        bigserial primary key,
+    batch_id          bigint not null,
+    model_name        varchar(100) not null,
+    layer_name        varchar(20),
+    started_at        timestamp,
+    finished_at       timestamp,
+    status            varchar(20),
+    rows_processed    bigint,
+    error_message     text
 );
 
 
---
+++ pagila_dbt_analytics/macros/audit_sql/log_model_start.sql
@@ -0,0 +1,32 @@
+{% macro log_model_start() %}
+
+{% set sql %}
+
+insert into audit.etl_run_log
+(
+    batch_id,
+    model_name,
+    layer_name,
+    started_at,
+    status
+)
+
+select
+    batch_id,
+    '{{ model.name }}',
+
+    split_part('{{ model.name }}','_',1),
+
+    current_timestamp,
+
+    'RUNNING'
+
+from audit.etl_batch
+
+where invocation_id='{{ invocation_id }}';
+
+{% endset %}
+
+{% do run_query(sql) %}
+
+{% endmacro %}
