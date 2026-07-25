acec907d0c06dabd486530fca04e647b1729a28d
Owais <awaisajaz1@gmail.com>
2026-07-25 16:25:53 +0500

    silver film added

---
+++ pagila_dbt_analytics/models/gold/fact/fact_rental.sql
@@ -0,0 +1,83 @@
+{{ config(
+    materialized='incremental',
+    unique_key='rental_id',
+    incremental_strategy='merge',
+    on_schema_change='fail'
+) }}
+
+with rental as (
+
+    select *
+    from {{ ref('silver_rental') }}
+
+),
+
+dim_customer as (
+
+    select
+        customer_key,
+        customer_id
+    from {{ ref('dim_customer') }}
+
+),
+
+dim_film as (
+
+    select
+        film_key,
+        film_id
+    from {{ ref('dim_film') }}
+
+),
+
+dim_store as (
+
+    select
+        store_key,
+        store_id
+    from {{ ref('dim_store') }}
+
+),
+
+inventory as (
+
+    select
+        inventory_id,
+        film_id,
+        store_id
+    from {{ ref('silver_inventory') }}
+
+)
+
+select
+
+    {{ dbt_utils.generate_surrogate_key(['r.rental_id']) }} as rental_key,
+    r.rental_id,
+    dc.customer_key,
+    df.film_key,
+    ds.store_key,
+    r.inventory_id,
+    r.rental_date,
+    r.return_date,
+
+    extract(day from (r.return_date - r.rental_date))
+        as rental_duration_days,
+
+    1 as rental_count,
+
+    r.last_update,
+    r.bronze_load_dts
+
+from rental r
+
+join inventory i
+    on r.inventory_id = i.inventory_id
+
+join dim_customer dc
+    on r.customer_id = dc.customer_id
+
+join dim_film df
+    on i.film_id = df.film_id
+
+join dim_store ds
+    on i.store_id = ds.store_id
