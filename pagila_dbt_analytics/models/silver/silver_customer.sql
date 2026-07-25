3355ba214c5dbee143163203ef365d39623f0645
Owais <awaisajaz1@gmail.com>
2026-07-25 15:23:24 +0500

    silver transformation started

--- pagila_dbt_analytics/models/silver/silver_customer.sql
+++ pagila_dbt_analytics/models/silver/silver_customer.sql
@@ -1,25 +1,4 @@
-with customer_cte as (
-
-    select
-        *,
-        row_number() over (
-            partition by customer_id
-            order by last_update desc
-        ) as rn
-    from {{ source('pagila_src', 'customer') }}
-
-)
-
-select
-    customer_id,
-    store_id,
-    first_name,
-    last_name,
-    email,
-    address_id,
-    activebool,
-    create_date,
-    last_update,
-    active
-from customer_cte
-where rn = 1
+{{ config(
+    materialized='incremental',
+    unique_key='customer_id'
+) }}
