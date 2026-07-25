e2c28d8cb16b4a5af422750c7dd24bf0d415d3d8
Owais <awaisajaz1@gmail.com>
2026-07-25 15:54:08 +0500

    silver film added

--- pagila_dbt_analytics/models/silver/silver_actors.sql
+++ pagila_dbt_analytics/models/silver/silver_actors.sql
@@ -1,7 +1,8 @@
 {{ config(
     materialized='incremental',
     unique_key='actor_id',
-    incremental_strategy='merge'
+    incremental_strategy='merge',
+    on_schema_change='fail'
 ) }}
 
 with src as (
--- pagila_dbt_analytics/models/silver/silver_film.sql
+++ pagila_dbt_analytics/models/silver/silver_film.sql
@@ -1,7 +1,8 @@
 {{ config(
     materialized='incremental',
     unique_key='film_id',
-    incremental_strategy='merge'
+    incremental_strategy='merge',
+    on_schema_change='fail'
 ) }}
 
 with src as (
