00ac118bdbf8a89686d46920a032c1d0f3a4f208
Owais <awaisajaz1@gmail.com>
2026-07-25 15:27:19 +0500

    silver customer added

--- pagila_dbt_analytics/models/silver/silver_customer.sql
+++ pagila_dbt_analytics/models/silver/silver_customer.sql
@@ -2,3 +2,41 @@
     materialized='incremental',
     unique_key='customer_id'
 ) }}
+
+with src as (
+
+    select *
+    from {{ ref('bronze_customer') }}
+
+    {% if is_incremental() %}
+    where last_update >
+        (
+            select coalesce(max(last_update),'1900-01-01')
+            from {{ this }}
+        )
+    {% endif %}
+
+),
+
+dedup as (
+
+    select
+        *,
+        row_number() over (
+            partition by customer_id
+            order by last_update desc
+        ) as rn
+    from src
+
+)
+
+select
+    customer_id,
+    first_name || ' ' || last_name as full_name,
+    email,
+    address_id,
+    activebool,
+    create_date,
+    last_update
+from dedup
+where rn = 1
